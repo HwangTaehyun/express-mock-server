172aa7e3307f9be08e6142d176f8bfcd762969c2
Taehyun Hwang <eeht1717@gmail.com>
2022-06-18 16:33:17 +0900

    [feat] Add cors option

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,26 @@
-import 'colors';
-import config from './config';
-import { App } from './app';
-import ws from 'ws';
+"use strict";
 
-const { port } = config;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.serverStart = serverStart;
+exports.serverStop = serverStop;
+
+require("colors");
+
+var _config = _interopRequireDefault(require("./config"));
+
+var _app = require("./app");
+
+var _ws = _interopRequireDefault(require("ws"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const {
+  port
+} = _config.default;
 let server,
-  wsServer = null;
-
+    wsServer = null;
 /**
  * Method to start server.
  *
@@ -17,8 +31,9 @@ let server,
  * @param serverConfig.webSocketEnabled false
  * @param serverConfig.cors false
  */
-export function serverStart(sources, serverConfig = {}) {
-  const app = new App(sources, serverConfig);
+
+function serverStart(sources, serverConfig = {}) {
+  const app = new _app.App(sources, serverConfig);
   const port = getServerPort(serverConfig);
   server = app.start(port, () => {
     console.log(`* Server port    ${port}`.yellow);
@@ -39,39 +54,42 @@ export function serverStart(sources, serverConfig = {}) {
 
   return server;
 }
-
 /**
  * Method to stop server.
  */
-export function serverStop() {
+
+
+function serverStop() {
   if (server) {
     server.close();
   }
+
   if (wsServer) {
     wsServer.close();
   }
 }
 
 function startWebSocket(server) {
-  const wsServer = new ws.Server({ noServer: true });
-  wsServer.on('connection', (socket) => {
+  const wsServer = new _ws.default.Server({
+    noServer: true
+  });
+  wsServer.on('connection', socket => {
     console.log('WebSocket client connected');
-    socket.on('message', (message) => {
+    socket.on('message', message => {
       console.log('WebSocket client message received');
       console.log(message);
     });
     socket.on('close', () => console.log('WebSocket client disconnected'));
   });
-
   server.on('upgrade', (request, socket, head) => {
-    wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
       wsServer.emit('connection', socket, request);
     });
   });
-
   return wsServer;
 }
 
 function getServerPort(serverConfig) {
   return serverConfig.port ? serverConfig.port : port;
 }
+//# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import { serverStart } from '../lib';
 import sources from '../test/sources';
 
-serverStart(sources, { port: 2121, webSocketEnabled: true });
+serverStart(sources, { port: 2121, webSocketEnabled: true, cors: true });
